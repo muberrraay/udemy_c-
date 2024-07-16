353791d07dc91558384c61f586b3263a0cc811b5
muberrraay <muberra_yerinde@outlook.com>
2024-07-16 14:13:14 +0300

    seventh post

---
+++ lesson3_loops/timer_uygulama/timer_uygulama/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer_uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+            timer1.Start();
+            
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+
+        {
+            i++;
+            label1.Text = i.ToString();
+            if (i == 30)
+            {
+                this.BackColor = Color.Yellow;
+            }
+            else if (i == 40)
+            {
+                this.BackColor = Color.Green;
+            }
+            else if (i== 70)
+            {
+                this.BackColor = Color.Red;
+                i = 0;
+            }
+        }
+    }
+}
---
+++ lesson5_methods/cube_volume_methods/cube_volume_methods/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cube_volume_methods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int cube(int a)
+        {
+            int hacim = a * a * a;
+            return hacim;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1= Convert.ToInt16(textBox1.Text);
+            
+            label2.Text=cube(sayi1).ToString();
+
+        }
+    }
+}
